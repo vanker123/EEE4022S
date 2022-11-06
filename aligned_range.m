065a685cea431c63782d2b025067018ba32f6079
vanker123 <117035531+vanker123@users.noreply.github.com>
2022-11-06 23:06:00 +0200

    Update aligned_range.m

--- aligned_range.m
+++ aligned_range.m
@@ -11,34 +11,35 @@ for i = [1:si(1)]
     [shift,lg]=xcorr(x,unaligned);
     [max_val max_ind]= max(shift);
     BinShiftsNeeded = max_ind - length(x);
-    Shifted_Profile=circshift(unaligned,BinShiftsNeeded);
-    AlignedProfiles_Matrix(i, :) = Shifted_Profile;
+    Shifted_Profile=circshift(unaligned,BinShiftsNeeded); %number of integer bin shifts required
+    AlignedProfiles_Matrix(i, :) = Shifted_Profile; 
     BinShiftsNeeded_vector(i) = BinShiftsNeeded;
 end
-%figure; plot(BinShiftsNeeded_vector)
+
+%figure; plot(BinShiftsNeeded_vector) %plotting integer bin shifts
 %xlabel("Range Profile")
 %ylabel("Integer bin shifts")
-%figure; imagesc(20*log10(AlignedProfiles_Matrix));
+%figure; imagesc(20*log10(AlignedProfiles_Matrix)); %range profiles after first stage of haywood range alignment
 %axis xy;
 %colormap('jet');
 %set(gcf,'color','w')
-%set(gca,'FontSize',11)
-%ylabel('Profile Number','fontsize',14);
-%xlabel('Range (m)','fontsize',14);
-%title('HRR profiles','fontsize',14);
+%set(gca,'FontSize',15)
+%ylabel('Profile Number','fontsize',15);
+%xlabel('Range (m)','fontsize',15);
+%title('HRR profiles','fontsize',15);
 
  %% Haywood Alignment algorithm
 
 NumberOfProfiles = size(unaligned_Range,1); % Assumption: each row is a range profile
 RangeProfilesVector = 1:1:NumberOfProfiles;
-order=1;
+order=1; %order of line of best fit
 BestFit=polyfit(RangeProfilesVector,BinShiftsNeeded_vector, order);
 BinShiftsNeededNonInteger = polyval(BestFit, RangeProfilesVector);
 
 %figure; 
 %plot(BinShiftsNeeded_vector,'-o');
 %hold on;
-%plot(RangeProfilesVector, BinShiftsNeededNonInteger,'-*r');
+%plot(RangeProfilesVector, BinShiftsNeededNonInteger,'-*r'); %plot of non integer bin shifts
 %xlabel("Range Profile")
 %ylabel("Non integer bin shift")
 %set(gcf,'color','w')
@@ -55,23 +56,23 @@ for j = 1:number_of_rows
     k = transpose(BinShiftsNeededNonInteger);
     
     Phi_vector = exp(-1i*2*pi*n.*k(j)/N);
-    Range_Profile_shifted = ifft(fft(HRR_profiles(j,:)).*Phi_vector);
+    Range_Profile_shifted = ifft(fft(HRR_profiles(j,:)).*Phi_vector); %Shifting profiles with reference profile
     HaywoodAligned(j,:)=Range_Profile_shifted;  
     
 end 
 
 %figure;
-Range_Alignment=HaywoodAligned;
-%imagesc(20*log10(abs(HaywoodAligned)));
+Range_Alignment=HaywoodAligned; %aligned HRR range profiles
+%imagesc(20*log10(abs(HaywoodAligned)));%plot of range profiles after haywood range alignment
 %xlabel('Range (m)');
 %ylabel("doppler frequency")
 %axis xy;
 %colormap('jet');
 %colorbar
-%set(gca,'FontSize',14)
+%set(gca,'FontSize',15)
 %set(gcf,'color','w')
-%ylabel('Profile Number','fontsize',14);
-%xlabel('Range (m)','fontsize',14);
-%title('HRR profiles','fontsize',14);
+%ylabel('Profile Number','fontsize',15);
+%xlabel('Range (m)','fontsize',15);
+%title('HRR profiles','fontsize',15);
 end 
 
